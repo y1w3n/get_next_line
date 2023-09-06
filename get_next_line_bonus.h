5c0502d2a1d080bca40a2e5f15732450be53fff0
Yi Wen Tay <126394197+y1w3n@users.noreply.github.com>
2023-09-06 22:07:12 +0800

    Add files via upload

---
+++ get_next_line_bonus.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yitay <yitay@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/05 16:57:40 by yitay             #+#    #+#             */
+/*   Updated: 2023/09/06 21:52:41 by yitay            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
+
+# include <unistd.h> //write
+# include <fcntl.h> //open and read
+# include <stdlib.h> //maloc
+# include <stdio.h> //printf
+
+//get_next_line main func
+char	*get_next_line(int fd);
+
+//get_next_line utils
+char	*ft_strchr(const char *s, int c);
+int		ft_strlen(const char *s);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strcpy(char *dst, const char *src);
+
+#endif
