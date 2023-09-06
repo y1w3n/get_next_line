0c2c4ef5ce53673f67c0ae2024396631dd2852e0
Yi Wen Tay <yitay@u80z04s03.42kl.edu.my>
2023-09-06 21:16:03 +0800

    mandatory

---
+++ get_next_line.c
@@ -0,0 +1,114 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yitay <yitay@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/05 17:16:49 by yitay             #+#    #+#             */
+/*   Updated: 2023/09/06 19:05:21 by yitay            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*read_it(int fd, char *txt)
+{
+	char	*tmp;
+	int		bytes;
+
+	bytes = 0;
+	tmp = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!tmp)
+		return (NULL);
+	while (!(ft_strchr (txt, '\n')))
+	{
+		bytes = read(fd, tmp, BUFFER_SIZE);
+		if (bytes <= 0)
+			break ;
+		tmp[bytes] = '\0';
+		txt = ft_strjoin(txt, tmp);
+	}
+	free (tmp);
+	if (bytes == -1)
+		return (NULL);
+	return (txt);
+}
+
+char	*extract_it(char *txt)
+{
+	int		i;
+	char	*line;
+
+	i = 0;
+	if (!txt || !txt[i])
+		return (NULL);
+	while (txt[i] != '\n' && txt[i])
+		i++;
+	line = malloc (sizeof(char) * (i + 2));
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (txt[i] != '\n' && txt[i])
+	{
+		line[i] = txt[i];
+		i++;
+	}
+	if (txt[i] == '\n')
+	{
+		line[i] = '\n';
+		i++;
+	}
+	line[i] = '\0';
+	return (line);
+}
+
+char	*extract_liao(char *txt)
+{
+	int		i;
+	char	*left_over;
+
+	i = 0;
+	if (!txt)
+		return (NULL);
+	while (txt[i] != '\n' && txt[i])
+		i++;
+	if (!txt[i])
+	{
+		free (txt);
+		return (NULL);
+	}
+	left_over = malloc(sizeof(char) * (ft_strlen(txt) - i));
+	if (!left_over)
+	{
+		free (left_over);
+		return (NULL);
+	}
+	ft_strcpy (left_over, &txt[i + 1]);
+	free (txt);
+	return (left_over);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*txt;
+	char		*line;
+	int			i;
+
+	i = 0;
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	txt = read_it (fd, txt);
+	line = extract_it (txt);
+	txt = extract_liao (txt);
+	return (line);
+}
+
+// int main()
+// {
+// 	int fd = open("hello.txt",O_RDONLY);
+// 	char *s;
+// 	s = get_next_line(fd);
+// 	printf("%d\n",fd);
+// 	printf("%s\n", s);
+// }
---
+++ get_next_line_utils.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yitay <yitay@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/05 17:20:08 by yitay             #+#    #+#             */
+/*   Updated: 2023/09/06 19:06:11 by yitay            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	char	*ptr;
+
+	ptr = (char *)s;
+	while (ptr && *ptr)
+	{
+		if (*ptr == (char)c)
+			return (ptr);
+		ptr++;
+	}
+	if (c == 0)
+		return (ptr);
+	return (NULL);
+}
+
+int	ft_strlen(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s && s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	int		i;
+	int		j;
+	char	*join;
+
+	i = 0;
+	j = 0;
+	join = malloc(ft_strlen(s1)+ft_strlen(s2) + 1);
+	if (!join)
+		return (NULL);
+	while (s1 && s1[i])
+	{
+		join[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+		join[i++] = s2[j++];
+	join[i] = '\0';
+	free((char *) s1);
+	return (join);
+}
+
+char	*ft_strcpy(char *dst, const char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src && src[i])
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
+}
